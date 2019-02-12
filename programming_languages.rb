bfbbb26f302678b9fae6cdec0b6c8b94cc20844c
Will Saffran <willh.saffran@gmail.com>
2019-02-12 01:09:08 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, lang_hash|
+    lang_hash.each do |lang, type_hash|
+      type_hash.each do |type, type_hash|
+        if new_hash.include?(lang)
+          new_hash[lang][:style] << style
+        else
+          new_hash[lang] = {:type => type_hash, :style => [style]}
+        end
+      end
+    end
+  end
+  new_hash
 end
+
+  
+
