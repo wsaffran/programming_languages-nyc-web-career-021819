0d4eeedfbe3672b1e7bd6d9d7ec9047cb3f064d9
Will Saffran <willh.saffran@gmail.com>
2019-02-12 01:06:54 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -7,6 +7,10 @@ def reformat_languages(languages)
           new_hash[lang][:style] << style
         else
           new_hash[lang] = {:type => type, :style => [style]}
+        end
+      end
+    end
+  end
 end
 
   
